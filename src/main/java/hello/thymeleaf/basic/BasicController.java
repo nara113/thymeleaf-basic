18a3e350e71d3eec3bcfb77b5e36a8275bc9f32f
nara113 <0554my@naver.com>
2022-05-05 19:04:24 +0900

    타임리프 - 기본 기능 - 주석

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -115,4 +115,10 @@ public class BasicController {
         addUsers(model);
         return "basic/condition";
     }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
 }
