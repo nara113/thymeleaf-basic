ab0987b56c0f0e50293ac30c9783302a323f4c56
nara113 <0554my@naver.com>
2022-05-05 18:35:11 +0900

    타임리프 - 기본 기능 - 속성 값 설정

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -89,4 +89,9 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/operation";
     }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
 }
