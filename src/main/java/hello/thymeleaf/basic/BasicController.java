27ddd275bb16de771a42f741a60f2a53fcda4a91
nara113 <0554my@naver.com>
2022-05-05 17:13:12 +0900

    타임리프 - 기본 기능 - 변수 - SpringEL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,14 @@
 package hello.thymeleaf.basic;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/basic")
 @Controller
@@ -18,5 +23,26 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User user1 = new User("userA", 20);
+        User user2 = new User("userB", 30);
+
+        List users = List.of(user1, user2);
+        Map<String, User> userMap = Map.of("userA", user1, "userB", user2);
+
+        model.addAttribute("user", user1);
+        model.addAttribute("users", users);
+        model.addAttribute("userMap", userMap);
+
+        return "basic/variable";
+    }
+
+    @AllArgsConstructor @Getter
+    static class User {
+        private String username;
+        private int age;
     }
 }
